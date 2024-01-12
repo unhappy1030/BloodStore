9e1471f0141c72f8c709b21e447c07f98ac13ab7
LeeHyangWoo <white021030@gmail.com>
2024-01-12 13:37:29 +0900

    [feat] <NewFamilyTreeTest> : Left FamilyTree complete

--- BloodStore/Assets/Scripts/LeeHyangWoo/NewFamilyTree/FamilyTree/TreeManager.cs
+++ BloodStore/Assets/Scripts/LeeHyangWoo/NewFamilyTree/FamilyTree/TreeManager.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.Assertions.Must;
+using UnityEngine.UIElements;
 
 public class TreeManagerTest : MonoBehaviour
 {
@@ -12,23 +14,27 @@ public class TreeManagerTest : MonoBehaviour
     public float pairOffSet = 0.2f;
     public float offSetX, offSetY;
     private float halfX, halfY;
-
-
+    private float pairSize, unit;
+    private float lastX, lastY;
     void Start()
     {
+        lastX = 0f;
+        lastY = 0f;
         if (pairSO.root.Count == 0)
         {
             Node node = new Node();
             node.SetAllRandom();
             AddFirstNode(node);
         }
-        SetHalfPrefab();
+        SetPrefab();
         MakeFamilyTree();
     }
 
-    void SetHalfPrefab(){
+    void SetPrefab(){
         halfX = nodePrefab.GetComponent<SpriteRenderer>().bounds.extents.x;
         halfY = nodePrefab.GetComponent<SpriteRenderer>().bounds.extents.y;
+        pairSize = pairOffSet + 4 * halfX;
+        unit = pairSize + offSetX;
     }
 
     void MakeFamilyTree(){
@@ -39,10 +45,19 @@ public class TreeManagerTest : MonoBehaviour
     void MakeChildren(Group rootGroup){
         if(rootGroup.pair.childNum != 0){
             rootGroup.childrenGroup = new();
-            List<Vector2> posList = MakeChildPosList(rootGroup.groupPos, rootGroup.pair.childNum, offSetX, offSetY);
+            List<Vector2> posList = MakeChildPosList(rootGroup.groupPos, rootGroup.pair.childNum);
             for(int i = 0; i < rootGroup.pair.childNum; i++){
                 Group group = MakeGroupObject();
                 group.pair = rootGroup.pair.children[i];
+                if(posList[i].y >= lastY && posList[i].x <= lastX){
+                    Debug.Log("Move");
+                    lastX += unit;
+                    posList[i] = new Vector2(lastX, posList[i].y);
+                }
+                Debug.Log("LastX : " + lastX.ToString() + "  name : " + group.pair.male.name);
+                lastX = posList[i].x;
+                lastY = posList[i].y;
+                Debug.Log("posX : " + posList[i].x.ToString() + " posY : " + posList[i].y.ToString());
                 group.groupPos = posList[i];
                 group.transform.position = group.groupPos;
                 group.leftPos =  group.groupPos + new Vector2(-1 * (halfX + (pairOffSet / 2)), 0);
@@ -95,11 +110,9 @@ public class TreeManagerTest : MonoBehaviour
     //     return display;
     // }
 
-    List<Vector2> MakeChildPosList(Vector2 rootPos, int childNum, float offSetX, float offSetY){
+    List<Vector2> MakeChildPosList(Vector2 rootPos, int childNum){
         List<Vector2> posList = new();
-        float pairSize = pairOffSet + 4 * halfX;
-        float unit = pairSize + offSetX;
-        float startPoint = rootPos.x - (childNum - 1) * unit / 2;
+        float startPoint = rootPos.x;
         for(int i = 0; i < childNum; i++){
             Vector2 pos = new Vector2(startPoint + unit * i, rootPos.y -1 * (halfY * 2 + offSetY));
             posList.Add(pos);
