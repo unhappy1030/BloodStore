777f4e50911baabbd347bb32cd75e21f8ae723f8
Ulim567 <ulim040125@gmail.com>
2024-01-24 14:53:52 +0900

    [feat] : NPCSO - combine day dialogue in conditions

--- BloodStore/Assets/Scripts/LeeUlim/Dialogue/DialogueControl.cs
+++ BloodStore/Assets/Scripts/LeeUlim/Dialogue/DialogueControl.cs
@@ -6,168 +6,168 @@ using Yarn;
 
 public class DialogueControl : MonoBehaviour
 {
-    int dayCount = 0;
-    int conditionCount = 0;
-    int totalCount = 0;
+//     int dayCount = 0;
+//     int conditionCount = 0;
+//     int totalCount = 0;
 
-    public List<NPCInfo> npcInfos; // assign at inspector
-    public Dictionary<string, int> npcConditions;
+//     public List<NPCInfo> npcInfos; // assign at inspector
+//     public Dictionary<string, int> npcConditions;
     
-    List<NPCInfo> ableNPCInfos;
-    List<DialogueInfo> dayDialogues;
-    List<DialogueInfo> condDialogues;
-    public List<DialogueInfo> ableDialogues;
+//     List<NPCInfo> ableNPCInfos;
+//     List<DialogueInfo> dayDialogues;
+//     List<DialogueInfo> condDialogues;
+//     public List<DialogueInfo> ableDialogues;
 
-    void Awake(){
-        ResetCondition();
-    }
+//     void Awake(){
+//         ResetCondition();
+//     }
 
-    void ResetCondition(){
-        npcConditions = new();
-        npcConditions.Clear();
+//     void ResetCondition(){
+//         npcConditions = new();
+//         npcConditions.Clear();
 
-        for(int i=0; i<npcInfos.Count; i++){
-            npcConditions.Add(npcInfos[i].npcName, 0);
-        }
+//         for(int i=0; i<npcInfos.Count; i++){
+//             npcConditions.Add(npcInfos[i].npcName, 0);
+//         }
 
-        Debug.Log("Reset Condition...");
-    }
+//         Debug.Log("Reset Condition...");
+//     }
 
-    public void GetAbleNPC(){
-        if(ableNPCInfos == null) // reset
-        {
-            ableNPCInfos = new();
-        }
-        else
-        {
-            ableNPCInfos.Clear();
-        }
+//     public void GetAbleNPC(){
+//         if(ableNPCInfos == null) // reset
+//         {
+//             ableNPCInfos = new();
+//         }
+//         else
+//         {
+//             ableNPCInfos.Clear();
+//         }
 
-        if(npcInfos == null || npcInfos.Count == 0){
-            Debug.Log("There is no NPC Information...");
-            return;
-        }
+//         if(npcInfos == null || npcInfos.Count == 0){
+//             Debug.Log("There is no NPC Information...");
+//             return;
+//         }
 
-        foreach(NPCInfo npcInfo in npcInfos){
-            if(npcInfo.AbleNPC(GameManager.Instance.day)){
-                ableNPCInfos.Add(npcInfo);
-            }
-        }
-    }
+//         foreach(NPCInfo npcInfo in npcInfos){
+//             if(npcInfo.AbleNPC(GameManager.Instance.day)){
+//                 ableNPCInfos.Add(npcInfo);
+//             }
+//         }
+//     }
 
-    public void GetDayDialogue(WhereNodeStart where, WhenNodeStart when){
-        if(dayDialogues == null) // reset
-        {
-            dayDialogues = new();
-        }
-        else
-        {
-            dayDialogues.Clear();
-        }
+//     public void GetDayDialogue(WhereNodeStart where, WhenNodeStart when){
+//         if(dayDialogues == null) // reset
+//         {
+//             dayDialogues = new();
+//         }
+//         else
+//         {
+//             dayDialogues.Clear();
+//         }
 
-        if(ableNPCInfos == null || ableNPCInfos.Count == 0){
-            Debug.Log("There is no able NPC in this day...");
-            return;
-        }
+//         if(ableNPCInfos == null || ableNPCInfos.Count == 0){
+//             Debug.Log("There is no able NPC in this day...");
+//             return;
+//         }
 
-        int index = 0;
+//         int index = 0;
 
-        foreach(NPCInfo ableNpcInfo in ableNPCInfos){
-            List<DayDialogue> list = ableNpcInfo.GetDayDialogues(where, when, GameManager.Instance.day);
+//         foreach(NPCInfo ableNpcInfo in ableNPCInfos){
+//             List<DayDialogue> list = ableNpcInfo.GetDayDialogues(where, when, GameManager.Instance.day);
 
-            dayCount += list.Count;
+//             dayCount += list.Count;
 
-            foreach(DayDialogue day in list){
-                dayDialogues.Add(new());
-                dayDialogues[index].npcName = ableNpcInfo.npcName;
-                dayDialogues[index].priority = day.priority;
-                dayDialogues[index].dialogueName = day.dialogueName;
-                Debug.Log("Able day name " + index + " : " + dayDialogues[index].npcName);
+//             foreach(DayDialogue day in list){
+//                 dayDialogues.Add(new());
+//                 dayDialogues[index].npcName = ableNpcInfo.npcName;
+//                 dayDialogues[index].priority = day.priority;
+//                 dayDialogues[index].dialogueName = day.dialogueName;
+//                 Debug.Log("Able day name " + index + " : " + dayDialogues[index].npcName);
 
-                index++;
-            }
-        }
-    }
+//                 index++;
+//             }
+//         }
+//     }
     
-    public void GetCondDialogue(WhereNodeStart where, WhenNodeStart when){
-        if(condDialogues == null) // reset
-        {
-            condDialogues = new();
-        }
-        else
-        {
-            condDialogues.Clear();
-        }
+//     public void GetCondDialogue(WhereNodeStart where, WhenNodeStart when){
+//         if(condDialogues == null) // reset
+//         {
+//             condDialogues = new();
+//         }
+//         else
+//         {
+//             condDialogues.Clear();
+//         }
 
-        if(ableNPCInfos == null || ableNPCInfos.Count == 0){
-            Debug.Log("There is no able NPC in this Condition...");
-            return;
-        }
+//         if(ableNPCInfos == null || ableNPCInfos.Count == 0){
+//             Debug.Log("There is no able NPC in this Condition...");
+//             return;
+//         }
 
-        int index = 0;
+//         int index = 0;
 
-        foreach(NPCInfo ableNpcInfo in ableNPCInfos){
-            string npcName = ableNpcInfo.npcName;
-            List<CondDialogue> list 
-                = ableNpcInfo.GetCondDialogues(where, when, npcConditions[npcName], GameManager.Instance.day);
+//         foreach(NPCInfo ableNpcInfo in ableNPCInfos){
+//             string npcName = ableNpcInfo.npcName;
+//             List<CondDialogue> list 
+//                 = ableNpcInfo.GetCondDialogues(where, when, npcConditions[npcName], GameManager.Instance.day);
 
-            conditionCount += list.Count;
+//             conditionCount += list.Count;
             
-            foreach(CondDialogue cond in list){
-                condDialogues.Add(new());
-                condDialogues[index].npcName = ableNpcInfo.npcName;
-                condDialogues[index].priority = cond.priority;
-                condDialogues[index].dialogueName = cond.dialogueName;
-                Debug.Log("Able Cond name " + index + " : " + condDialogues[index].npcName);
-                index++;
-            }
-        }
-    }
+//             foreach(CondDialogue cond in list){
+//                 condDialogues.Add(new());
+//                 condDialogues[index].npcName = ableNpcInfo.npcName;
+//                 condDialogues[index].priority = cond.priority;
+//                 condDialogues[index].dialogueName = cond.dialogueName;
+//                 Debug.Log("Able Cond name " + index + " : " + condDialogues[index].npcName);
+//                 index++;
+//             }
+//         }
+//     }
 
-    public void SetAllDialogues(){
-        if(ableDialogues == null) // reset
-        {
-            ableDialogues = new();
-        }
-        else
-        {
-            ableDialogues.Clear();
-        }
+//     public void SetAllDialogues(){
+//         if(ableDialogues == null) // reset
+//         {
+//             ableDialogues = new();
+//         }
+//         else
+//         {
+//             ableDialogues.Clear();
+//         }
 
-        foreach(DialogueInfo day in dayDialogues){
-            ableDialogues.Add(day);
-            Debug.Log("Able day name : " + day.npcName);
-        }
+//         foreach(DialogueInfo day in dayDialogues){
+//             ableDialogues.Add(day);
+//             Debug.Log("Able day name : " + day.npcName);
+//         }
 
-        foreach(DialogueInfo cond in condDialogues){
-            ableDialogues.Add(cond);
-            Debug.Log("Able Cond name : " + cond.npcName);
-        }
+//         foreach(DialogueInfo cond in condDialogues){
+//             ableDialogues.Add(cond);
+//             Debug.Log("Able Cond name : " + cond.npcName);
+//         }
 
-        foreach(DialogueInfo info in ableDialogues){
-            Debug.Log("Name : " + info.npcName.ToString());
-        }
-        ableDialogues.Sort(ComparePriority);
-    }
+//         foreach(DialogueInfo info in ableDialogues){
+//             Debug.Log("Name : " + info.npcName.ToString());
+//         }
+//         ableDialogues.Sort(ComparePriority);
+//     }
 
-    // 내림차순
-    int ComparePriority(DialogueInfo dialogue1, DialogueInfo dialogue2){
-        if(dialogue1.priority > dialogue2.priority)
-            return -1;
-        else if(dialogue1.priority < dialogue2.priority)
-            return 1;
-        else
-            return 0;
-    }
+//     // 내림차순
+//     int ComparePriority(DialogueInfo dialogue1, DialogueInfo dialogue2){
+//         if(dialogue1.priority > dialogue2.priority)
+//             return -1;
+//         else if(dialogue1.priority < dialogue2.priority)
+//             return 1;
+//         else
+//             return 0;
+//     }
 
-    public void SetCondition(string name, int condition){
-        npcConditions[name] = condition;
-        Debug.Log("Set "+ name.ToString() + " Condition to " + condition.ToString() + "...");
-    }
+//     public void SetCondition(string name, int condition){
+//         npcConditions[name] = condition;
+//         Debug.Log("Set "+ name.ToString() + " Condition to " + condition.ToString() + "...");
+//     }
 }
 
-public class DialogueInfo{
-    public string npcName;
-    public int priority;
-    public string dialogueName;
-}
+// public class DialogueInfo{
+//     public string npcName;
+//     public int priority;
+//     public string dialogueName;
+// }
