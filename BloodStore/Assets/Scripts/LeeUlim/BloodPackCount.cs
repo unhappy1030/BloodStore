d4052741a083742eca849a04c4e02f253264f37f
Ulim567 <ulim040125@gmail.com>
2024-02-01 15:58:04 +0900

    [feat] <Test> : Ready to show count for multiple categories, not yet

--- BloodStore/Assets/Scripts/LeeUlim/BloodPackCount.cs
+++ BloodStore/Assets/Scripts/LeeUlim/BloodPackCount.cs
@@ -17,11 +17,11 @@ public class BloodPackCount : MonoBehaviour
 
         texts[0].text = bloodPacks.categoryNum["Male"].ToString();
         texts[1].text = bloodPacks.categoryNum["Female"].ToString();
-        texts[2].text = bloodPacks.categoryNum["A"].ToString();
-        texts[3].text = bloodPacks.categoryNum["B"].ToString();
-        texts[4].text = bloodPacks.categoryNum["AB"].ToString();
-        texts[5].text = bloodPacks.categoryNum["O"].ToString();
-        texts[6].text = bloodPacks.categoryNum["-"].ToString();
-        texts[7].text = bloodPacks.categoryNum["+"].ToString();
+        texts[2].text = bloodPacks.categoryNum["+"].ToString();
+        texts[3].text = bloodPacks.categoryNum["-"].ToString();
+        texts[4].text = bloodPacks.categoryNum["A"].ToString();
+        texts[5].text = bloodPacks.categoryNum["B"].ToString();
+        texts[6].text = bloodPacks.categoryNum["AB"].ToString();
+        texts[7].text = bloodPacks.categoryNum["O"].ToString();
     }
 }
