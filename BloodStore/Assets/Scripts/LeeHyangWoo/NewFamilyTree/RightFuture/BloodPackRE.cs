c8f1437204bdd07981f6f1d7ffc79cce594492a5
unhappy1030 <white021030@gmail.com>
2024-02-01 11:51:39 +0900

    [fix] BloodPacks Error complete

---
+++ BloodStore/Assets/Scripts/LeeHyangWoo/NewFamilyTree/RightFuture/BloodPackRE.cs
@@ -0,0 +1,36 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using System.IO;
+
+// [System.Serializable]
+// public class SaveBloodPackArray{
+//     public BloodPack[] arr;
+// }
+
+// public class BloodPackRE : MonoBehaviour
+// {
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+        
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+        
+//     }
+// }
+
+// [System.Serializable]
+// public class BloodPack
+// {
+//     public Node node;
+//     public int num;
+//     public int date;
+//     public bool existNext;
+//     public BloodPackLink Deserialize(){
+//         return new BloodPackLink(this);
+//     }
+// }
