37029ece77e924fdfaf63900101c3d8467ae6ab3
Ulim567 <ulim040125@gmail.com>
2024-03-22 13:53:55 +0900

    [feat] <Start> : MoneyUI - Finish ShowMoneyTextAnimation method = create new tmp, show animation, and destroy

--- BloodStore/Assets/Scripts/UI/MoneyUI/MoneyUI.cs
+++ BloodStore/Assets/Scripts/UI/MoneyUI/MoneyUI.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEditor.Tilemaps;
 
 public class MoneyUI : MonoBehaviour
 {
     public GameObject moneyCanvas;
     public GameObject moneyPanel;
-    public TextMeshProUGUI moneyText;
-    public TextMeshProUGUI ratingText;
+    public GameObject textParent;
+    public GameObject moneyText;
+    public GameObject ratingText;
 
     private void OnEnable()
     {
@@ -38,16 +40,52 @@ public class MoneyUI : MonoBehaviour
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 
-    public void ShowMoneyTextAnimation(float num){
-        moneyText.gameObject.SetActive(true);
-        moneyText.text = num.ToString();
-        moneyText.gameObject.SetActive(false);
+    void Start(){
+        StartCoroutine(Test());
+
     }
 
-    public void ShowRatingTextAnimation(float num){
-        ratingText.gameObject.SetActive(true);
-        ratingText.text = num.ToString();
-        ratingText.color = (num >= 2.5) ? Color.green : Color.red;
-        ratingText.gameObject.SetActive(false);
+    IEnumerator Test(){
+        yield return new WaitForSeconds(5);
+        StartCoroutine(ShowMoneyTextAnimation(300));
+        yield return new WaitForSeconds(2);
+        StartCoroutine(ShowMoneyTextAnimation(-300));
+        yield return new WaitForSeconds(0.5f);
+        StartCoroutine(ShowMoneyTextAnimation(-400000000));
     }
+
+    public IEnumerator ShowMoneyTextAnimation(float num){
+        GameObject newText = Instantiate(moneyText);
+        newText.SetActive(false);
+        newText.transform.SetParent(textParent.transform);
+
+        TextMeshProUGUI newTmp = newText.GetComponent<TextMeshProUGUI>();
+        if(newTmp == null){
+            Debug.Log("There is no TMP in new moneyText...");
+            yield break;
+        }
+
+        newTmp.text = (num > 0)? "+" + num.ToString("N0") : num.ToString("N0");
+        newTmp.color = (num > 0)? Color.green : Color.red;
+        Debug.Log("newTmp.color : " + newTmp.color);
+
+        Animator newAnim = newText.GetComponent<Animator>();
+        if(newAnim == null){
+            Debug.Log("There is no Animator in new moneyText...");
+            yield break;
+        }
+
+        newText.SetActive(true);
+        yield return new WaitForSeconds(2);
+        
+        newText.SetActive(false);
+        Destroy(newText);
+    }
+
+    // public void ShowRatingTextAnimation(float num){
+    //     ratingText.gameObject.SetActive(true);
+    //     ratingText.text = num.ToString();
+    //     ratingText.color = (num >= 2.5) ? Color.green : Color.red;
+    //     ratingText.gameObject.SetActive(false);
+    // }
 }
