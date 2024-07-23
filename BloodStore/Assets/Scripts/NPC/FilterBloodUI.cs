b37032fcb25c5c08ff7bb4a42c42d516b7f9af1a
Ulim567 <ulim040125@gmail.com>
2024-07-23 16:05:18 +0900

    [refactor] : rename BloodFiltering -> FilterBloodUI and refactor

--- BloodStore/Assets/Scripts/NPC/FilterBloodUI.cs
+++ BloodStore/Assets/Scripts/NPC/FilterBloodUI.cs
@@ -2,7 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BloodFiltering : MonoBehaviour
+/// <summary>
+/// 혈액 필터링 과정과 UI 관련
+/// </summary>
+public class FilterBloodUI : MonoBehaviour
 {
     public BloodSellProcessManager bloodSellProcess; // assign at inspector
 
@@ -10,10 +13,17 @@ public class BloodFiltering : MonoBehaviour
         gameObject.SetActive(false);
     }
 
+    /// <summary>
+    /// UI 켜짐(InteractObjInfo)과 동시에 FilterBlood 코루틴 시작
+    /// </summary>
     void OnEnable(){
         StartCoroutine(FilterBlood());
     }
 
+    /// <summary>
+    /// 1.5초 기다린 후 자기 자신 setActive false
+    /// </summary>
+    /// <returns></returns>
     IEnumerator FilterBlood(){
         yield return new WaitForSeconds(1.5f);
         bloodSellProcess.FilterBlood();
